9c4628c9bbb007996c8f3e441a8746c0e347fe22
themandalore <fett.nicholas@yahoo.com>
2019-11-22 12:38:34 -0500

    removeAPI

--- index.js
+++ index.js
@@ -10,5 +10,5 @@ const PORT = process.env.PORT || 4000
 const app = express();
 
 app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use('/api',require('./routes/api'))
+app.use('/',require('./routes/api'))
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
