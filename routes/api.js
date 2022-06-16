cd4302bb54e85fc644f1b2ca3178235eba8fafbf
lauren gregory <lmg2@illinois.edu>
2022-06-16 08:39:25 -0500

    update to flex functions as well as expand to more networks
    updated to flex functions, need to adjust documentation, add more networks once addresses are available. Changed .env to only need one input for infura key

--- routes/api.js
+++ routes/api.js
@@ -3,30 +3,49 @@ const router = express.Router();
 // const { ethers } = require("hardhat");
 var Web3 = require('web3');
 var fs = require('fs');
-var web3, tellorMaster, tellorMaster, tellorLens, tellorGovernance, tellorOracle
+var web3, tellorFlex, tellorLens, tellorGovernance, tellorMaster
 
 function useNetwork(netName, res) {
 	// "Web3.providers.givenProvider" will be set if in an Ethereum supported browser.
 	try {
 		console.log(process.cwd())
+		const flexABI = JSON.parse(fs.readFileSync("contracts/tellorFlex.json"));
 		const masterABI = JSON.parse(fs.readFileSync("contracts/tellorMaster.json"));
-		const lensABI = JSON.parse(fs.readFileSync("contracts/tellorLens.json"));
 		const governanceABI = JSON.parse(fs.readFileSync("contracts/tellorGovernance.json"));
-		const oracleABI = JSON.parse(fs.readFileSync("contracts/tellorOracle.json"));
+		//const oracleABI = JSON.parse(fs.readFileSync("contracts/tellorOracle.json"));
 
+
+        //ADD: ropsten, goerli, harmony, kovan
 		switch (netName) {
 			case "rinkeby":
-				web3 = new Web3(process.env.nodeURLRinkeby || Web3.givenProvider);
-				tellorMaster = new web3.eth.Contract(masterABI, '0x88dF592F8eb5D7Bd38bFeF7dEb0fBc02cf3778a0');
-				tellorLens = new web3.eth.Contract(lensABI, '0x16aC7874290B086D37D1B1ABC18153A8dCe8A335');
+				web3 = new Web3("https://rinkeby.infura.io/v3/" + process.env.infura_key || Web3.givenProvider);
+				tellorFlex = new web3.eth.Contract(flexABI, '0x095869B6aAAe04422C2bdc6f185C1f2Aba41EA6B');
+				tellorGovernance = new web3.eth.Contract(governanceABI, '0x3eb81A11DD28Fe8ED9f53D1456248aC86d5893C6');
 				break;
-			default:
-				netName = "mainnet"
-				web3 = new Web3(process.env.nodeURL || Web3.givenProvider);
-				tellorMaster = new web3.eth.Contract(masterABI, '0x88dF592F8eb5D7Bd38bFeF7dEb0fBc02cf3778a0');
-				tellorGovernance = new web3.eth.Contract(governanceABI, '0x51d4088d4EeE00Ae4c55f46E0673e9997121DB00');
-				tellorLens = new web3.eth.Contract(lensABI, '0xd259A9F7d5b263C400284e9544C9c0088c481cfd');
-				tellorOracle = new web3.eth.Contract(oracleABI, '0xe8218cACb0a5421BC6409e498d9f8CC8869945ea');
+
+            case "polygon":
+				web3 = new Web3("https://polygon-mainnet.infura.io/v3/" + process.env.infura_key || Web3.givenProvider);
+				tellorFlex = new web3.eth.Contract(flexABI, '0xFd45Ae72E81Adaaf01cC61c8bCe016b7060DD537');
+				tellorGovernance = new web3.eth.Contract(governanceABI, '0x98458269081eD05bA58babE3f004E46625C8D9F2');
+				break;
+
+			case "polygon-mumbai":
+				web3 = new Web3("https://polygon-mumbai.infura.io/v3/" + process.env.infura_key || Web3.givenProvider);
+				tellorFlex = new web3.eth.Contract(flexABI, '0x41b66dd93b03e89D29114a7613A6f9f0d4F40178');
+				tellorGovernance = new web3.eth.Contract(governanceABI, '0x8A868711e3cE97429faAA6be476F93907BCBc2bc');
+				break;
+
+            case "arbitrum-rinkeby":
+				web3 = new Web3("https://arbitrum-rinkeby.infura.io/v3/" + process.env.infura_key || Web3.givenProvider);
+				tellorFlex = new web3.eth.Contract(flexABI, '0xbB97C038c338c3DcAF06D5be3B4A3e0B24835f9C');
+				tellorGovernance = new web3.eth.Contract(governanceABI, '0x3eb81A11DD28Fe8ED9f53D1456248aC86d5893C6');
+				break;
+
+			case "optimism-kovan":
+				web3 = new Web3("https://optimism-kovan.infura.io/v3/" + process.env.infura_key || Web3.givenProvider);
+				tellorFlex = new web3.eth.Contract(flexABI, '0xa0013274d34a7c469952379646F26aA1C1237131');
+				tellorGovernance = new web3.eth.Contract(governanceABI, '0x3eb81A11DD28Fe8ED9f53D1456248aC86d5893C6');
+				break;
 		}
 		console.log("using network:", netName)
 	} catch (e) {
@@ -49,20 +68,24 @@ function processInput(filename, json) {
 	});
 }
 
+
 // Get general Tellor state data and saves the data under data/state.json
 router.get('/:netName?/info', async function (req, res) {
 	try {
 		useNetwork(req.params.netName, res)
 		console.log('getting all variable information...')
 		//read data from Tellor's contract
-		var _stakerCount = await tellorMaster.methods.getUintVar(web3.utils.keccak256("_STAKE_COUNT")).call();
+		var _stakeAmount = await tellorLens.methods.getStakeAmount().call();
+		var _amountStaked = await tellorLens.methods.getTotalStakeAmount().call();
+		var _stakerCount = _amountStaked / _stakeAmount
 		var _disputeCount = await tellorGovernance.methods.getVoteCount().call();
-		var _totalSupply = await tellorMaster.methods.getUintVar(web3.utils.keccak256("_TOTAL_SUPPLY")).call();
-		var _timeOfLastValue = await tellorOracle.methods.getTimeOfLastNewValue().call();
+		//var _totalSupply = await tellorMaster.methods.getUintVar(web3.utils.keccak256("_TOTAL_SUPPLY")).call();
+		var _timeOfLastValue = await tellorFlex.methods.getTimeOfLastNewValue().call();
 		res.send({
+			stakeAmount: _stakeAmount,
+			amountStaked: _amountStaked,
 			stakerCount: _stakerCount,
 			disputeCount: _disputeCount,
-			total_supply: _totalSupply,
 			timeOfLastNewValue: _timeOfLastValue,
 		})
 
@@ -71,13 +94,11 @@ router.get('/:netName?/info', async function (req, res) {
 		var state = "state";
 		state = {
 			timeChecked: _now,
+			stakeAmount: _stakeAmount,
+			amountStaked: _amountStaked,
 			stakerCount: _stakerCount,
-			difficulty: _difficulty,
-			//currentRequestId: _currentRequestId,
 			disputeCount: _disputeCount,
-			total_supply: _totalSupply,
 			timeOfLastNewValue: _timeOfLastValue,
-			//requestCount: _requestCount
 		}
 		var jsonStats = JSON.stringify(state);
 		let filename = "public/state.json";
@@ -99,7 +120,7 @@ router.get('/:netName?/price/:queryID/:count?', async function (req, res) {
 		}
 		var queryID = req.params.queryID
 		var scale = queryID === "0x000000000000000000000000000000000000000000000000000000000000000a" ? 1e18 : 1e6;
-		console.log('getting last', reqCount, 'prices for request ID', queryID);
+		console.log('getting last', reqCount, 'prices for queryID', queryID);
 		var r = await tellorLens.methods.getLastValues(queryID, reqCount).call()
 		var results = [];
 		for (let index = 0; index < r.length; index++) {
@@ -116,48 +137,13 @@ router.get('/:netName?/price/:queryID/:count?', async function (req, res) {
 	}
 })
 
-//Get latest data for all data IDs
-router.get('/:netName?/prices/:queryIds?/:count?', async function (req, res) {
-	try {
-		useNetwork(req.params.netName, res)
-		var reqCount = req.params.count
-		// reqCount is optional so set to 1 when undefined.
-		if (reqCount == undefined) {
-			reqCount = 1
-		}
-        var queryIds0 = req.params.queryIds;
-        var queryIds = queryIds0.split("-");
-		console.log('getting last', reqCount, 'prices for following data IDs:', queryIds);
-
-		var r = await tellorLens.methods.getLastValuesAll(reqCount, queryIds).call()
-		//console.log(r);
-		var results = [];
-		for (let index = 0; index < r.length; index++) {
-			if (+r[index].value != 0) {
-			    var scale = r[index].meta.id === "0x000000000000000000000000000000000000000000000000000000000000000a" ? 1e18 : 1e6;
-			    var val = web3.utils.hexToNumberString(r[index].value) / scale;
-				results.push({
-					timestamp: r[index].timestamp,
-					value: val,
-					id: r[index].meta.id,
-				    tip: r[index].tip,
-				})
-			}
-
-		};
-		res.send(results);
-	} catch (e) {
-		let err = e.message
-		res.send({ err });
-	}
-})
 
 //Get data for a specific dispute
 router.get('/:netName?/dispute/:disputeID', async function (req, res) {
 	try {
 		useNetwork(req.params.netName, res)
 		console.log('getting dispute info...', req.params.disputeID);
-		var _returned = await tellorGovernance.methods.getDisputeInfo(req.params.disputeID).call();
+		var _returned = await tellorLens.methods.getDisputeInfo(req.params.disputeID).call();
 		res.send({
 			queryId: _returned[0],
 			timestamp: _returned[1],
@@ -175,7 +161,7 @@ router.get('/:netName?/dispute/:disputeID', async function (req, res) {
 router.get('/:netName?/getDisputeFee', async function (req, res) {
 	try {
 		useNetwork(req.params.netName, res)
-		let disputeFee = Web3.utils.fromWei(await tellorMaster.methods.getUintVar(web3.utils.keccak256('_DISPUTE_FEE')).call());
+		let disputeFee = await tellorLens.methods.getDisputeFee().call();
 		res.send({ disputeFee })
 	} catch (e) {
 		let err = e.message
@@ -185,14 +171,17 @@ router.get('/:netName?/getDisputeFee', async function (req, res) {
 
 
 //Get data for a specific dispute
-router.get('/:netName?/getStakerInfo/:address', async function (req, res) {
+router.get('/:netName?/StakerInfo/:address', async function (req, res) {
 	try {
 		useNetwork(req.params.netName, res)
-		var resp = await tellorMaster.methods.getStakerInfo(req.params.address).call();
+		var resp = await tellorFlex.methods.getStakerInfo(req.params.address).call();
 		console.log(resp);
 		res.send({
-			status: resp[0],
-			stakeDate: resp[1],
+			stakeDate: resp[0],
+			stakedBalance: resp[1],
+			lockedBalance; resp[2],
+			reporterLastTimestamp: resp[3],
+			reportsSubmitted: resp[4]
 		})
 	} catch (e) {
 		let err = e.message
